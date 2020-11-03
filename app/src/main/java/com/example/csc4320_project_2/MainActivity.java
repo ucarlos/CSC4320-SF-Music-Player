f6e368aefe594c3fad9d1a39747ea7c3125834aa
Ulysses Carlos <ulycarlos@gmail.com>
2020-11-02 22:54:44 -0500

    Default Constructor for DatabaseTag works now
    Signed-off-by: Ulysses Carlos <ulycarlos@gmail.com>

--- app/src/main/java/com/example/csc4320_project_2/MainActivity.java
+++ app/src/main/java/com/example/csc4320_project_2/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.csc4320_project_2;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 
+import com.example.csc4320_project_2.taglib.DatabaseTag;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.RequiresApi;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -16,16 +22,46 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    protected void testDatabaseTag() throws ReadOnlyFileException, CannotReadException, TagException, InvalidAudioFrameException, IOException {
+        System.out.println("****************************************************************");
+        System.out.println("WHAT THE FUCK DID YOU JUST FUCKING SAY ABOUT ME YOU LITTLE BITCH");
+        System.out.println("****************************************************************");
+        // Test Taglib Here:
+        DatabaseTag dt = new DatabaseTag(getApplicationContext());
+        dt.print_tags();
+
+        System.out.println("****************************************************************");
+        System.out.println("NOW CARRY ON!");
+        System.out.println("****************************************************************");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Set here:
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +81,21 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        try {
+            testDatabaseTag();
+        } catch (ReadOnlyFileException e) {
+            e.printStackTrace();
+        } catch (CannotReadException e) {
+            e.printStackTrace();
+        } catch (TagException e) {
+            e.printStackTrace();
+        } catch (InvalidAudioFrameException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/csc4320_project_2/taglib/DatabaseTag.java
@@ -0,0 +1,80 @@
+package com.example.csc4320_project_2.taglib;
+import android.content.Context;
+import android.content.res.AssetManager;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
+
+public class DatabaseTag {
+    // Public Section
+
+    public DatabaseTag(Context context) throws IOException, TagException, ReadOnlyFileException, CannotReadException, InvalidAudioFrameException {
+        // Acess Information in assets/
+        my_context = context;
+        AssetManager am = my_context.getAssets();
+        InputStream is = am.open("default_audio_file.ogg");
+        // Now create a file out of it:
+
+        byte temp_byte[] = new byte[1];
+
+        ArrayList<Byte> read_buffer = new ArrayList<>();
+        int bytes_read;
+        while ((bytes_read = is.read(temp_byte)) != -1){
+            read_buffer.add(temp_byte[0]);
+        }
+
+        byte output_buffer[] = new byte[read_buffer.size()];
+        for (int i = 0; i < output_buffer.length; i++) {
+            output_buffer[i] = read_buffer.get(i);
+        }
+
+        // Now store into temp file:
+
+        file = File.createTempFile("Temp_File", ".ogg");
+        OutputStream os = new FileOutputStream(file);
+        os.write(output_buffer);
+        audio_file = AudioFileIO.read(file);
+
+    }
+
+    public DatabaseTag(String file_path) throws TagException, ReadOnlyFileException, CannotReadException, InvalidAudioFrameException, IOException {
+        this.file_path = file_path;
+        audio_file = AudioFileIO.read(new File(file_path));
+    }
+
+    public final AudioFile get_audio_file() { return audio_file; }
+    public final String get_file_path() { return file_path; }
+
+    public void print_tags() {
+        Tag tag = audio_file.getTag();
+
+        System.out.println("Title: " + tag.getFirst(FieldKey.TITLE));
+        System.out.println("Track Number: " + tag.getFirst(FieldKey.TRACK));
+        System.out.println("Disc Number: " + tag.getFirst(FieldKey.DISC_NO));
+        System.out.println("Artist: " + tag.getFirst(FieldKey.ARTIST));
+        System.out.println("Album Artist: " + tag.getFirst(FieldKey.ALBUM_ARTIST));
+        System.out.println("Album: " + tag.getFirst(FieldKey.ALBUM));
+        System.out.println("Year: " + tag.getFirst(FieldKey.YEAR));
+        System.out.println("Genre: " + tag.getFirst(FieldKey.GENRE));
+        System.out.println("");
+
+
+    }
+    // Private Section
+    private String file_path;
+    private AudioFile audio_file;
+    private File file;
+    private Context my_context;
+}
